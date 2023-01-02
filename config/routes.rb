41d6aa70d852d4a23ba9922e889cf06fb53f4cb4
EC2 Default User <ec2-user@ip-172-31-14-60.ap-northeast-1.compute.internal>
2023-01-02 16:16:59 +0000

    forth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+  devise_for :admin, skip: [:registrations, :passwords],　controllerscontrollerscontrollers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :end_users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions',
+    passwords: "public/passwords"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
