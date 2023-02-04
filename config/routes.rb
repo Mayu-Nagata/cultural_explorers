82f0ab1c58b48e3589da8a6ff5727cf974f07362
EC2 Default User <ec2-user@ip-172-31-14-60.ap-northeast-1.compute.internal>
2023-02-04 00:29:55 +0000

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :passwords],　controllerscontrollerscontrollers: {
-    sessions: "admin/sessions"
-  }
+
   devise_for :end_users, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions',
     passwords: "public/passwords"
   }
 
+  devise_for :admin, skip: [:registrations, :passwords],　controllers: {
+    sessions: "admin/sessions"
+  }
 
   scope module: :public do
     root to: 'homes#top'
@@ -31,22 +32,16 @@ Rails.application.routes.draw do
 
   end
 
-  namespace :public do
-   #get 'end_users' => 'end_users#index'
-    #get 'end_users/:id' => 'end_users#show'
-    #get 'end_users/information/edit' => 'end_users#edit'
-    #patch 'end_users/information' => 'end_users#update'
-    #get 'end_users/unsubscribe' => 'end_users#unsubscribe'
-    #patch 'end_users/withdraw' => 'end_users#withdraw'
-    #resources :posts, only: [ :index, :new, :show, :create, :destroy]
-    #resources :likes, only: [ :index]
+  scope moddule: :admin do
+    get 'admin' => 'homes#top'
+
   end
 
   namespace :admin do
-    get 'admin' => 'homes#top'
+
     resources :end_users, only: [ :index, :show, :edit, :update]
     resources :posts, only: [ :show, :destroy]
-  end
+ end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_29_152156) do
+ActiveRecord::Schema.define(version: 2023_01_22_035144) do
 
   create_table "Favorites", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2023_01_29_152156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
+    t.index ["name"], name: "index_end_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
