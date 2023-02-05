0856ff049d2c74ab5d93408fbc78f887ace62c5a
EC2 Default User <ec2-user@ip-172-31-14-60.ap-northeast-1.compute.internal>
2023-02-05 03:20:39 +0000

    add admin edit

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -15,13 +15,15 @@ class Admin::EndUsersController < ApplicationController
     @end_user = EndUser.find(params[:id])
 
     @end_user.update(end_user.params)
+    redirect_to admin_end_user_path
+
   end
 
 
   private
 
   def end_user_params
-    params.require(:end_user).permit(:name,:profile_image,:email)
+    params.require(:end_user).permit(:name, :profile_image, :email, :is_deleted)
 
   end
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,17 @@
 class Admin::HomesController < ApplicationController
   def top
+    @posts = Post.all
   end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+
+  end
+
+  def end_user_params
+    params.require(:end_user).permit( :name, :introduction, :profile_image)
+  end
+  
 end
