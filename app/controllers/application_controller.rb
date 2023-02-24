1f4384df2f7a595c5029329d1c78296e07751fbf
EC2 Default User <ec2-user@ip-172-31-14-60.ap-northeast-1.compute.internal>
2023-02-24 10:26:21 +0000

    add validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_end_user!, except: [:top, :about, :guest_sign_in]
+
+
+  before_action :authenticate_admin!, if: :admin_url
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
+
+
 end
